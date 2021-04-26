2acd9876ad030ff1c7a351af7b0297312ad91666
DenysPhV <77440293+DenysPhV@users.noreply.github.com>
2021-04-26 11:04:22 +0000

    Deploying to gh-pages from @ DenysPhV/project_group_4@f939d069a80a8dfed8ec07a785e7e749c3b705c2 🚀

--- src.f5e938c8.js
+++ src.f5e938c8.js
@@ -5,4 +5,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/mobile-menu");
 },{"./sass/main.scss":"clu1","./js/mobile-menu":"sByI"}]},{},["Focm"], null)
-//# sourceMappingURL=/project_group_4/src.cddf4adc.js.map
+//# sourceMappingURL=/project_group_4/src.f5e938c8.js.map
